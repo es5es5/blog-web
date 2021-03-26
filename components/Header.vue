dbd93e35c7f46d4022451e8ebc1bfbbf4f8548cc
es5es5 <es5e5s@nate.com>
2021-03-26 18:30:39 +0900

    init

---
+++ components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="header" :class="scrolling ? 'scrolling' : ''">
+    Header
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Header',
+  data () {
+    return {
+      scrolling: false
+    }
+  },
+  beforeMount () { window.addEventListener('scroll', this.handleScroll) },
+  destroyed () { window.removeEventListener('scroll', this.handleScroll) },
+  methods: {
+    handleScroll () {
+      if (window.scrollY > 50) {
+        this.scrolling = true
+      } else {
+        this.scrolling = false
+      }
+    },
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+#header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 5000;
+  transition: background-color .5s;
+
+  &.scrolling { background-color: #fff; }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Header />
+    <div class="abc">
+      a
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import Header from '~/components/Header'
+
+@Component({
+  components: {
+    Header
+  }
+})
+export default class extends Vue {
+  authorName: string = 'JHyeok'
+}
+</script>
+
+<style lang="scss" scoped>
+.abc {
+  height: 150vh;
+}
+</style>
