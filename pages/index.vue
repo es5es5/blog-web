5f27e4db509d12821b2c662996e81a5971e5c532
es5es5 <es5e5s@nate.com>
2021-03-29 10:31:45 +0900

    shims-vue.d

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
-import Header from '~/components/Header'
+import Header from '@/components/Header'
 
 @Component({
   components: {
